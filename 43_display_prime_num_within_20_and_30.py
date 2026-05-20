14357d2f8d4bc84676b7a5b973ce95fea4b4a61d
ABHAY RAJ <4abhayraj@gmail.com>
2026-05-20 20:03:13 +0530

    16th may 2026 assignment complete

--- 43_display_prime_num_within_20_and_30.py
+++ 43_display_prime_num_within_20_and_30.py
@@ -1,9 +1,14 @@
-a=20
-b=30
+def is_prime(n):
+    if n<=1:
+        return False
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
 
-for i in range(a,b):
+primes=[]
+for n in range (20,30):
+    if is_prime(n):
+        primes.append(n)
+print(primes)
 
-    if i%2==0:
-        break
-    else:
-        print(i)
