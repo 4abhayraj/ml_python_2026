62a973999123fb8ea9a0c4b5e35931c58121bddd
ABHAY RAJ <4abhayraj@gmail.com>
2026-05-20 21:08:50 +0530

    def were added in ssome qs

--- 25_check_if_palindrome_no.py
+++ 25_check_if_palindrome_no.py
@@ -1,16 +1,16 @@
 #check if a number is palindrome or not
-
-num=int(input("Enter a number: "))
-
-text=str(num)
-print(len(text))
-my_bool=None
-for i in range (0,len(text)):
-    if (text[i]==text[-i-1]):
-        my_bool=True
+def palindrome(num):
+    text=str(num)
+#    print(len(text))
+    my_bool=None
+    for i in range (0,len(text)):
+        if (text[i]==text[-i-1]):
+            my_bool=True
+        else:
+            my_bool=False
+    if my_bool==True:
+        print(f"The {num} is palindrome")
     else:
-        my_bool=False
-if my_bool==True:
-    print("The given number is palindrome")
-else:
-    print("The given number is not palindrome")
+        print(f"The {num} is not palindrome")
+palindrome(121)
+palindrome(343)
--- 26_find_factorial.py
+++ 26_find_factorial.py
@@ -1,7 +1,9 @@
-#find factorial of anumber
-num=int(input("Enter anumber: "))
-sum=1
-for i in range(num,0,-1):
-    sum=sum*i
+#find factorial of a number
+def factorial(num):
+    sum=1
+    for i in range(num,0,-1):
+        sum=sum*i
 
-print(f"factorial of {num} is: {sum}")
+    print(f"factorial of {num} is: {sum}")
+factorial(5)
+factorial(10)
