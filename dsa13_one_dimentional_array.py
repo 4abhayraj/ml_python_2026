3de8976825e39e5f1e7afb6380b3c849947585ae
ABHAY RAJ <4abhayraj@gmail.com>
2026-06-04 01:32:34 +0530

    array complete

---
+++ dsa13_one_dimentional_array.py
@@ -0,0 +1,21 @@
+from numpy import *
+
+
+zero = array(10)
+print(zero)
+
+#one dimentional array
+one = array([1,2,3,4,5])
+print(one)
+
+#two dimentional array
+
+two = array([[1,2,3], [4,5,6]])
+print(two)
+
+two1 = array([[1,2,3],[4,5,6],[7,8,9]])
+
+print("\n")
+#three dimentional 
+three = array([[[1,2], [3,4]],[[5,6],[7,8]],[[9,10],[11,12]]])
+print(three)
